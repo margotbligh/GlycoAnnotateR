211382622eea0c51ccb6ffdba701783e0741a593
Margot Bligh <mbligh@mpi-bremen.de>
2023-03-28 18:03:55 +0200

    testing

--- inst/sugarMassesPredict-r_package-maldi_20230317.py
+++ inst/sugarMassesPredict-r_package-maldi_20230317.py
@@ -541,7 +541,7 @@ def predict_sugars(dp= [1, 6], polarity='neg', scan_range=[175, 1400], pent_opti
             bad_cols = ['k', 'x', 'rows', 'index', 'H', 'Na', 'Ca']
             masses_polyanionic_loop = masses_polyanionic_loop.drop(columns=bad_cols)
             #pivot
-            masses_polyanionic_loop = pd.pivot_table(masses_polyanionic_loop, columns='ion', values='mz', index=['name', 'mass', 'formula'])
+            masses_polyanionic_loop = pd.pivot_table(masses_polyanionic_loop, columns='ion', values='mz', index=['name'])
             return(masses_polyanionic_loop)
             masses_polyanionic_loop['name'] = masses_polyanionic_loop.index
             masses_polyanionic_loop = masses_polyanionic_loop.reset_index(drop=True)

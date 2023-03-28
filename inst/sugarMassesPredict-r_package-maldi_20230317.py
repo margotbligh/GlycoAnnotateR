af5ed3a85b253b433e59aa37da1b95da88ac713a
Margot Bligh <mbligh@mpi-bremen.de>
2023-03-28 16:42:09 +0200

    fixed typo

--- inst/sugarMassesPredict-r_package-maldi_20230317.py
+++ inst/sugarMassesPredict-r_package-maldi_20230317.py
@@ -529,14 +529,14 @@ def predict_sugars(dp= [1, 6], polarity='neg', scan_range=[175, 1400], pent_opti
                                           masses_polyanionic_loop.Na.astype(str) + 'Na+' + \
                                           masses_polyanionic_loop.Ca.astype(str) + 'Ca]-'
             masses_polyanionic_loop['mz'] = masses_polyanionic_loop.mass + (masses_polyanionic_loop.H * ion_mdiff['H']) + (masses_polyanionic_loop.Na * ion_mdiff['Na']) + (masses_polyanionic_loop.Ca * ion_mdiff['Ca']) + e_mdiff
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\.0", "", regex=True) #remove .0
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+0Na", "+", regex=True) #remove 0
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+0Ca", "", regex=True) #remove 0
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("-1H", "-H", regex=True) #replace 1H with H
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+1N", "+N", regex=True) #replace 1H with H
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+1C", "+C", regex=True) #replace 1H with H
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+]", "]", regex=True) #+] with ]
-            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\\+\\+", "+", regex=True) #++ with +
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\.0", "", regex=True) #remove .0
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+0Na", "+", regex=True) #remove 0
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+0Ca", "", regex=True) #remove 0
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\-1H", "-H", regex=True) #replace 1H with H
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+1N", "+N", regex=True) #replace 1H with H
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+1C", "+C", regex=True) #replace 1H with H
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+]", "]", regex=True) #+] with ]
+            masses_polyanionic_loop.ion = masses_polyanionic_loop.ion.str.replace("\+\+", "+", regex=True) #++ with +
             #drop extra columns
             bad_cols = ['k', 'x', 'rows', 'index', 'H', 'Na', 'Ca']
             masses_polyanionic_loop = masses_polyanionic_loop.drop(columns=bad_cols)
